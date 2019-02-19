06737596ec0fe28ff98cfbc2e855729763955b65
scarab5q <jmdenny1@sheffield.ac.uk>
2019-02-19 00:47:19 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+extern crate fst;
+extern crate fst_levenshtein;
+extern crate fst_regex;
+extern crate futures_io;
+
+use futures_io::iovec::IoVec;
+use std::env;
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+
+    let mut data = vec![];
+}
