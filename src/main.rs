936aa899ade607a554f986eb732781e8219980e8
scarab5q <jmdenny1@sheffield.ac.uk>
2019-02-21 17:28:03 +0000

    trying out async stdin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 extern crate fst;
 extern crate fst_levenshtein;
 extern crate fst_regex;
-extern crate futures_io;
-
-use futures_io::iovec::IoVec;
-use std::env;
+extern crate futures;
+extern crate tokio;
+use tokio::io;
+// use tokio::io::BufReader;
+use std::io::BufReader;
+use tokio::io::{stdin, stdout};
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let reader = BufReader::new(stdin);
+    let buffer = Vec::new();
 
-    let mut data = vec![];
+    let fut = io::read_until(reader, b'\n', buffer)
+        .and_then(move |(stdin, buffer)| stdout.write_all(&buffer).map_err(|e| panic!(e)))
+        .map_err(|e| panic!(e));
+
+    let mut stdin = io::stdin();
+    tokio::run(fut);
 }
