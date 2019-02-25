23327802597ad2f854880eb30590a33477024425
scarab5q <jmdenny1@sheffield.ac.uk>
2019-02-25 11:59:39 +0000

    managed to read stdin to a vec of strings

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,23 @@ extern crate fst;
 extern crate fst_levenshtein;
 extern crate fst_regex;
 extern crate futures;
-extern crate tokio;
-use tokio::io;
+// extern crate tokio;
+// use tokio::io;
 // use tokio::io::BufReader;
-use std::io::BufReader;
-use tokio::io::{stdin, stdout};
+// use std::io::BufReader;
+// use tokio::io::{Stdin, Stdout};
+use std::io::{self, BufRead};
+
+fn read_stdin() {
+    let stdin = io::stdin();
+    let mut keys = Vec::new();
+    for line in stdin.lock().lines() {
+        let key: String = line.unwrap();
+        keys.push(key);
+    }
+    println!("{:?}", keys);
+}
 
 fn main() {
-    let reader = BufReader::new(stdin);
-    let buffer = Vec::new();
-
-    let fut = io::read_until(reader, b'\n', buffer)
-        .and_then(move |(stdin, buffer)| stdout.write_all(&buffer).map_err(|e| panic!(e)))
-        .map_err(|e| panic!(e));
-
-    let mut stdin = io::stdin();
-    tokio::run(fut);
+    read_stdin();
 }
